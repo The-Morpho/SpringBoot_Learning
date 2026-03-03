39ea36ce4cab7aa7337e0f0cc2172f1821bdb995
The-Morpho <nasserwalid752@gmail.com>
2026-03-03 02:09:35 +0100

    Creating the first springboot Hello_API exercise (project)

---
+++ Ex01_Hello_API/src/main/java/com/todo/ex01helloapi/Ex01HelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.todo.ex01helloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01HelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01HelloApiApplication.class, args);
+    }
+
+}
---
+++ Ex01_Hello_API/src/test/java/com/todo/ex01helloapi/Ex01HelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.ex01helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01HelloApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
